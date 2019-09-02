87610d91f7cd3c2366e7f3cda9878a8e93b41931
Vilmar <vilmar.junior@edu.sc.senac.br>
2019-09-02 08:42:57 -0300

    Inclusão de INSERTs de endereços.

--- scripts/exerciciosaula05.sql
+++ scripts/exerciciosaula05.sql
@@ -42,5 +42,12 @@ ADD CONSTRAINT `id`
 ALTER TABLE `exemplos`.`telefone` 
 ADD COLUMN `idCliente` INT NULL AFTER `ativo`;
 
+INSERT INTO `endereco` (`id`,`cep`,`estado`,`cidade`,`bairro`,`numero`,`rua`) VALUES (1,'43294870','SC','S�o Jos�','Centro','189','Rua 51');
+INSERT INTO `endereco` (`id`,`cep`,`estado`,`cidade`,`bairro`,`numero`,`rua`) VALUES (2,'38476242','SC','Lages','Centro','515','Rua  79');
+INSERT INTO `endereco` (`id`,`cep`,`estado`,`cidade`,`bairro`,`numero`,`rua`) VALUES (3,'78529663','SC','Florian�polis','Centro','804','Rua  81');
+INSERT INTO `endereco` (`id`,`cep`,`estado`,`cidade`,`bairro`,`numero`,`rua`) VALUES (4,'39152270','SC','Lages','Centro','640','Rua 28');
+INSERT INTO `endereco` (`id`,`cep`,`estado`,`cidade`,`bairro`,`numero`,`rua`) VALUES (5,'78015242','SC','Florian�polis','Centro','126','Rua  6');
+INSERT INTO `endereco` (`id`,`cep`,`estado`,`cidade`,`bairro`,`numero`,`rua`) VALUES (6,'41857751','SC','Chapec�','Centro','125','Rua 66');
+
 
   
